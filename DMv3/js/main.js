02aa62507e4360b91f475747b8c3cfbaa6e06000
zao <z454236029@gmail.com>
2017-10-12 18:54:35 +0800

    DMv3

--- DMv3/js/main.js
+++ DMv3/js/main.js
@@ -47,7 +47,7 @@ $('.dropdown-menu').on("click","a",function(e){
 
 	var val = $(this).attr("data-val");
 
-	$(this).parents(".btn-group").attr("data-val",val).find("button").text(val);
+	$(this).parents(".btn-group").attr("data-val",val).find("button").html(val+' <span class="caret"></span>');
 
 });
 
@@ -72,7 +72,7 @@ $('#normal-submit-btn').on("click",function(e){
 
 	for( var i = 0; i < num; i++ ){
 
-		var rand = Math.random() * 100 >> 0;
+		var rand = Math.random() * 220 >> 0;
 
 		normal.push({
 			text : text,
@@ -111,5 +111,126 @@ video.ontimeupdate = function(e){
 video.ondurationchange = function(e){
 	DMer = DanMuer(wrapper,{});
 };
+
+/***************************************************************************************************/
+
+//修改全局样式
+
+$("#normal-change-btn").on("click",function(e){
+
+	var fontSize = $("#normal-danmu-size").val() + "px" || "20px",
+		fontStyle = $('#normal-danmu-fontStyle').attr("data-val"),
+		fontWeight = $('#normal-danmu-fontWeight').attr("data-val"),
+		color = $("#normal-danmu-color").val() || "#FFFFFF",
+		opacity = $("#normal-danmu-opacity").val() / 100 || 1,
+		family = $("#normal-danmu-family").attr("data-val"),
+		graType = $("#normal-danmu-gradient-type").attr("data-val"),
+		graSX = $("#normal-danmu-gradient-startX").val() * 1,
+		graSY = $("#normal-danmu-gradient-startY").val() * 1,
+		graEX = $("#normal-danmu-gradient-endX").val() * 1,
+		graEY = $("#normal-danmu-gradient-endY").val() * 1,
+		graSColor = $("#normal-danmu-gradient-startColor").val() || "#FFFFFF",
+		graEColor = $("#normal-danmu-gradient-endColor").val() || "#FFFFFF",
+		checked = $("#use-gradient")[0].checked;
+
+	DMer.changeStyle({
+		fontSize : fontSize,
+		fontFamily : family,
+		fontStyle : fontStyle,
+		fontWeight : fontWeight,
+		fontColor : color,
+		opacity : opacity
+	});
+
+	if( !checked ) return false;
+
+	DMer.addGradient(graType,{
+		startX : graSX,
+		startY : graSY,
+		endX : graEX,
+		endY : graEY,
+		colorStops : [{
+			"point" : 0,
+			"color" : graSColor
+		},{
+			"point" : 1,
+			"color" : graEColor
+		}]
+	});
+
+});
+
+/***************************************************************************************************/
+
+//其他控制选项
+
+//启动
+$("#all-start").on("click",function(e){
+
+	DMer.start();
+
+});
+//停止
+$("#all-stop").on("click",function(e){
+
+	DMer.stop();
+
+});
+//运行
+$("#all-play").on("click",function(e){
+
+	DMer.run();
+
+});
+//暂停
+$("#all-pause").on("click",function(e){
+
+	DMer.pause();
+
+});
+//清除
+$("#all-clear").on("click",function(e){
+
+	DMer.clear();
+
+});
+//重置
+$("#all-reset").on("click",function(e){
+
+	DMer.reset(0);
+
+});
+//禁用
+$("#all-disable").on("click",function(e){
+
+	DMer.disableEffect();
+
+});
+//启用
+$("#all-disable").on("click",function(e){
+
+	DMer.enableEffect();
+
+});
+//FPS
+var timer,
+	$msg = $(".showMsg");
+$("#all-fps").on("click",function(e){
+
+	clearInterval(timer);
+	timer = setInterval(function(){
+		$msg.html(DMer.getFPS());
+	},500);
+
+});
+//设置大小
+$("#all-resize").on("click",function(){
+
+	var $w = $("#canvas-danmu-width").val() * 1,
+		$h = $("#canvas-danmu-height").val() * 1;
+
+	DMer.setSize($w,$h);
+
+});
 
 });
