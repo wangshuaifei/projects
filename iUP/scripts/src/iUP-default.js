91043284cab135946e11a5925da14ea04f01fa8b
zao <z454236029@gmail.com>
2017-10-25 19:44:29 +0800

    projects

--- iUP/scripts/src/iUP-default.js
+++ iUP/scripts/src/iUP-default.js
@@ -2,6 +2,7 @@ define(function(){
 
 	var iup,
 		box = document.querySelector(".iUP-images-box"),
+		upBtn = document.querySelector(".iUP-btn"),
 		fileEle = document.getElementById("iUP-choose");
 
 	function init(){
@@ -13,6 +14,36 @@ define(function(){
 
 		};
 
+		box.onclick = function(e){
+
+			var tar = e.target,
+				clist = tar.classList,
+				hasClass = false;
+
+			for( var i = 0, list; list = clist[i++]; ){
+				if( list == "iUP-close" ){
+					hasClass = true;
+					break;
+				}
+			}
+
+			if(!hasClass) return false;
+
+			var item = tar.parentNode,
+				id = item.querySelector(".iUP-image").getAttribute("data-id");
+
+			iup.delete(id);
+			
+			item.classList.push("iUP-hide");
+
+		};
+
+		upBtn.onclick = function(e){
+
+
+
+		};
+
 	}
 
 	function readFiles(files){
@@ -21,8 +52,8 @@ define(function(){
 			iup.input(file,i,function(data){
 				var tpl = '<div class="iUP-item">'+
 					'<div class="iUP-layer iUP-hide"></div>'+
-					'<div class="iUP-image">'+
-						'<img src="'+data.dataUrl+'" data-id="'+data.id+'">'+
+					'<div class="iUP-image" data-id="'+data.id+'">'+
+						'<img src="'+data.dataUrl+'">'+
 					'</div>'+
 					'<div class="iUP-close iUP-hide">X</div>'+
 				'</div>';
